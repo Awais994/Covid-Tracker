036b7250f5c03e04fec59154d83fda224f7f9643
Muhammad Awais <59797068+Awais994@users.noreply.github.com>
2022-05-30 13:41:00 +0500

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards';
+import Header from './components/Header/index';
+import FetchData from './components/api/fetchData';
+import { useEffect, useState } from 'react';
+import { Api } from '@mui/icons-material';
+import BarChart from './components/Charts/BarChart';
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Chart } from 'react-chartjs-2'
+import CountryPicker from './components/CounryPicker/CountryPicker';
 
 function App() {
+  const [data, setData] = useState({
+    deaths: {
+      value: 0
+    },
+    recovered: {
+      value: 0
+    },
+    confirmed: {
+      value: 0
+    }
+
+  })
+  const [country,setCountry]=useState('')
+
+  useEffect(() => {
+    async function getApi() {
+      let { data: {confirmed, recovered, deaths} } = await FetchData();
+      setData({confirmed, recovered, deaths})
+    }
+    getApi()
+  }, [])
+
+  
+   const  handleChange = async (country) => {
+    const data = await FetchData(country);
+    setCountry(country)
+    setData(data)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header />
+      <div className='countrypic'>
+
+        <CountryPicker handleChange={handleChange}/>
+      </div>
+      <Cards data={data} country={country} />
+      <BarChart data={data} />
     </div>
   );
 }
---
+++ src/componets/info/Infogrid.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { useEffect, useState } from 'react';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  marginTop: 40,
+
+  color: theme.palette.text.secondary,
+}));
+
+export default function GlobalStats( ) {
+
+
+
+    let [covidApi , setCovidApi] = useState({})
+
+    useEffect(()=>{
+        async function getData(){
+            const response = await fetch("https://api.covid19api.com/summary");
+            const data = await response.json();
+            delete data.ID;
+            delete data.Message;
+            delete data.Global.Date
+            setCovidApi(data.Global)
+
+        }
+        getData()
+    },[]);
+  return (
+    <Box  sx={{ flexGrow: 1}}>
+      <Grid style={{maxWidth: 1000, margin: '0 auto'  }} container spacing={2}>
+          {
+              Object.keys(covidApi).map((objKey, ind)=>{
+                  return(
+                    <Grid item xs={12} sm={4}  >
+                    <Item elevation={5}> <h3 style={{textTransform: 'uppercase', color:'#26acd9'}}>{objKey.replace(/([A-Z])/g, ' $1')}</h3>
+                    <h3 style={{color: '#d92626'}}>{covidApi[objKey]}</h3>
+                    </Item>
+                  </Grid>
+
+
+                  )
+
+
+              })
+          }
+{/* 
+        <Grid item xs={12} sm={4}>
+          <Item elevation={5}>xs=4</Item>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Item elevation={5}>xs=4</Item>
+        </Grid> */}
+      </Grid>
+    </Box>
+  );
+}
+
+
